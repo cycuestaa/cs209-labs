e2e82582371ec697146f52e8e324dc62674026c6
siddsach <ssachdeva@uchicago.edu>
2017-10-24 13:23:29 -0500

    half done with part 1

--- src/edu/virginia/engine/display/DisplayObjectContainer.java
+++ src/edu/virginia/engine/display/DisplayObjectContainer.java
@@ -11,17 +11,48 @@ import javax.imageio.ImageIO;
  * A very basic display object for a java based gaming engine
  * 
  * */
-public class DisplayObject extends DisplayObject {
+public class DisplayObjectContainer extends DisplayObject {
 
 	/* All DisplayObject have a unique id */
 	private String id;
 
-    /*DisplayObjectContainers have a parent*/
-    private DisplayObject parent; 
-
-    /*DisplayObjectContainers have a parent*/
+    /*DisplayObjectContainers have children*/
     public ArrayList<DisplayObject> children;
 
 	/* The image that is displayed by this object */
 	private BufferedImage displayImage;
+
+    public void DisplayObjectContainer(String id) {
+        this.setId(id);
+        this.position= new Point(0,0);
+        this.pivotPoint= new Point (0,0);
+        this.Rotation=0;
+        this.visible=true;
+        this.alpha=1.0f;
+        this.oldAlpha=0.0f;
+        this.scaleX=1.0;
+        this.scaleY=1.0;
+    }
+
+
+    public void setChildren(ArrayList<DisplayObject> newChildren) {
+        this.children = newChildren;
+    }
+
+    public void addChild(DisplayObject child) {
+        this.children.add(child);
+    }
+    public void addChildAtIndex(DisplayObject child, int ind) {
+        this.children.add(ind, child);
+    }
+    public void removeChild(String id) {
+        for (int i=0; i<this.children.size(); i++) {
+            DisplayObject child = this.children.get(i);
+            if (child.getId() == id) {
+                this.removeByIndex(i);
+            }
+    }
+    public void removeByIndex(int ind) {
+        this.children.remove(ind);
+    }
 }
